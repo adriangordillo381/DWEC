ec2ae899a85981489f1911f8921ee8da4535c38f
adriangordillo381 <agordillob05@suarezdefigueroa.es>
2025-09-30 20:38:39 +0200

    Arrays1

---
+++ Js/Tema2/EjerciciosArray/Ejercicio1.js
@@ -0,0 +1,15 @@
+/*Crea un programa que pida al usuario 6 números y luego los muestre en orden inverso
+ (pista: usa un array para almacenarlos y "for" para mostrarlos)*/
+
+let array = new Array();
+let cont = 0;
+
+while (cont <= 6) {
+  let num = parseInt(prompt("Introduce un numero"));
+  array.push(num);
+  cont++;
+}
+
+for (i = array.length - 1; i > 0; i--) {
+  alert("El elemento " + i + " toma el valor " + array[i]);
+}
---
+++ Js/Tema2/EjerciciosArray/Ejercicio2.js
@@ -0,0 +1,5 @@
+/*
+2. Crea un programa que pregunte al usuario cuantos números va a introducir (por ejemplo, 5), le
+pida todos esos números, los guarde en un array y finalmente calcule y muestre la media de esos
+números
+*/
