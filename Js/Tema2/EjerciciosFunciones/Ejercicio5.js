45299feec803bb6670ce40f9c027c4ca465b01b0
adriangordillo381 <agordillob05@suarezdefigueroa.es>
2025-10-22 22:12:17 +0200

    .

---
+++ Js/Tema2/EjerciciosFunciones/Ejercicio5.js
@@ -0,0 +1,4 @@
+/*
+Recorrer la cadena hola#que#tal con un for usando split y sin join 
+. Guardando el resultado en un array usando split y join
+*/
---
+++ Js/Tema2/EjerciciosFunciones/Ejercicio6.js
@@ -0,0 +1,5 @@
+/*
+1. Mostrar los elementos de un array del final al principio usando pop
+*/
+
+let array = [1, 2, 3, 4, 5];
