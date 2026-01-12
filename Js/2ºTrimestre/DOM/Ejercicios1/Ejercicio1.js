6d49a3fe6e905e75bbc72a7cc89c64b41b0ca7ad
adriangordillo381 <agordillob05@suarezdefigueroa.es>
2026-01-12 22:10:43 +0100

    ok

---
+++ Js/2ºTrimestre/DOM/Ejercicios1/Ejercicio1.js
@@ -0,0 +1,11 @@
+// 1. Selecciona el elemento h1 por su ID y muestra su contenido en consola
+
+function mejecutar() {
+  const titulo = document.querySelector("#titulo");
+  console.log(titulo); // asi solo muestra la etiqueta con ese id
+
+  const primerIntro = document.querySelector("p.intro");
+  alert(primerIntro.textContent); //con textContent accede al contenido de la etiqueta
+
+  const todosIntro = document.querySelector();
+}
---
+++ Js/2ºTrimestre/ExpresionesRegulares/script.js
@@ -0,0 +1,15 @@
+/*
+function validarTelefono(){
+    const regex = /^[6-7]\d{8}$/
+    return regex.test[numeroTelefono];
+}
+
+function extraerHashTag(texto){
+    const regex = /#\w+/g;
+    return regex.match(texto);
+}
+
+function validarHora(){
+    const regex /^(0[0-9]|1[0-9]|2[0-3])\:[0-5][0-9]\:[0-5][0-9]$/;
+}
+    */
