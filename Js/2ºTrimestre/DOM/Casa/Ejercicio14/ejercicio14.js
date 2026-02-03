1ab1e30022f71e3890e3ac99f9fe3afc87d50d42
adriangordillo381 <agordillob05@suarezdefigueroa.es>
2026-02-04 00:40:35 +0100

    ok

--- Js/2ºTrimestre/DOM/Casa/Ejercicio14/ejercicio14.js
+++ Js/2ºTrimestre/DOM/Casa/Ejercicio14/ejercicio14.js
@@ -16,31 +16,32 @@ const productos= document.querySelectorAll(".producto")
 let count = 0
 
 boton.addEventListener("click", () => {
-
+    // Con el select index cogemos el indice de la seleccion escogida y despues lo obtenemos del array con select.options[].value
     const opcion = select.selectedIndex
-    const opcionSeleccionada = select.options[opcion].value
+    const opcionSeleccionada = select.options[opcion].value     /*En este ejercicio en vez de centrarnos en  mostrar en en                                                           producto que es nos centramos en ocultar los que no son*/
+
+
 
     const marcado = document.querySelector("#solo-disponibles:checked")
 
     productos.forEach(function(producto,indice){
-        if(opcionSeleccionada != producto.dataSet.categoria &&
-            opcionSeleccionada !=""
-            
-        ){
+        if(opcionSeleccionada != producto.dataSet.categoria && //Si la opcion seleccionada no es igual a la categoria Y si
+            opcionSeleccionada !=""){                            // no es la opcion todas añademelo a la clase oculto
+
 
             producto.classList.add("oculto")
         
 
         }else{
-            producto.classList.remove("oculto")
+            producto.classList.remove("oculto")//Si SI es la opcion seleccionada borrame todas las demas clases que estan añadidas en oculto
         }
 
         if(marcado && producto.dataset.stock<=0){
-            producto.classList.add("oculto")
+            producto.classList.add("oculto") // Si solo disponibles esta marcado y es menor que cero añademelo a la clase oculto
         }
 
         if(!producto.classList.contains("oculto")){
-            count++
+            count++ //Si el producto no contiene la clase oculto se cuenta para mostrar el numero en consola
         }
     })
 
