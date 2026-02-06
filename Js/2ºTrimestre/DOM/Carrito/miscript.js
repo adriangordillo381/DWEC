ea79f3df74e0a09ca58a492b347a73d314b92cb3
adriangordillo381 <agordillob05@suarezdefigueroa.es>
2026-02-06 18:42:01 +0100

    ok

---
+++ Js/2ºTrimestre/DOM/Carrito/miscript.js
@@ -0,0 +1,61 @@
+/**
+ * Datos de entrada: Array de productos serializados (Simulando una fuente externa)
+ * Formato: "IDProducto;Nombre;Precio;Descripción;URL_Imagen"
+ */
+const arrayProductos = [
+  "123;Monitor UltraWide;349.99;Monitor curvo de 34 pulgadas ideal para programar.;https://images.unsplash.com/photo-1527443224154-c4a3942d3acf?w=500",
+  "245;Teclado Mecánico;120.50;Teclado RGB con switches blue para una escritura táctil.;https://images.unsplash.com/photo-1511467687858-23d96c32e4ae?w=500",
+  "367;Ratón Ergonómico;55.00;Ratón inalámbrico diseñado para largas jornadas de trabajo.;https://images.unsplash.com/photo-1527864550417-7fd91fc51a46?w=500",
+  "423;Auriculares Studio;89.00;Cancelación de ruido activa y sonido de alta fidelidad.;https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=500",
+  "518;Silla Gaming;199.00;Comodidad extrema para tus sesiones de código.;https://www.powerplanetonline.com/cdnassets/silla_gaming_813_blanco_negro_001v2_l.jpg",
+  "696;Webcam 4K;75.25;Resolución Ultra HD para tus reuniones de equipo.;https://resource.logitech.com/w_80,h_50,c_limit,q_auto,f_auto,dpr_2.0/d_transparent.gif/content/dam/logitech/en/products/webcams/brio/gallery/brio-gallery-2.png?v=1",
+];
+
+//  array de Strings a array de objetos
+let productos = arrayProductos.map((item) => {
+  const partes = item.split(";");
+
+  return {
+    id: parseInt(partes[0]),
+    nombre: partes[1],
+    precio: parseFloat(partes[2]),
+    descripcion: partes[3],
+    imagen: partes[4],
+  };
+});
+
+const carrito = [];
+
+const productGrid = document.querySelector("#productGrid");
+const plantilla = document.querySelector("#plantillaTarjeta");
+
+function dibujarProducto(producto) {
+  const clon = plantilla.content.cloneNode(true);
+  const nombre = clon.querySelector(".product.title");
+  nombre.textContent = producto.nombre;
+  const descripcion = clon.querySelector(".product-desc");
+  descripcion.textContent = descripcion.nombre;
+  const precio = clon.querySelector(".product-price");
+  precio.textContent = producto.precio;
+  const imagen = clon.querySelector("product-image");
+  imagen.src = producto.imagen;
+
+  const botonId = document.querySelector(".add-btn");
+  botonId.dataset.id = producto.id;
+}
+
+function añadirProducto(producto) {
+  const productoExiste = carrito.find(function (product) {
+    return (product.id = producto.id);
+  });
+  if (productoExiste) {
+    productoExiste.id++;
+  } else {
+    productos.push(
+      producto.id,
+      producto.nombre,
+      producto.precio,
+      producto.descripcion,
+    );
+  }
+}
